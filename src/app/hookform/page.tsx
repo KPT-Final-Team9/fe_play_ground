c7b9f989a58b7f1373aeb53fd0a4c2d77038607e
kano <ajvls98@gmail.com>
2024-04-23 14:13:37 +0900

    Co-authored-by: Seungmi Woo <wSeungMi@github.com>

--- src/app/hookform/page.tsx
+++ src/app/hookform/page.tsx
@@ -24,11 +24,36 @@ export default function Page() {
     getValues,
     watch,
     resetField,
+    clearErrors,
+    getFieldState,
+    setValue,
+    trigger,
     formState: { errors, isValid }
   } = useForm<IFormInput>({ criteriaMode: 'all' })
 
   const onSubmit: SubmitHandler<IFormInput> = data => {
     console.log(data)
+  }
+
+  const handleClick = async () => {
+    try {
+      // trigger 메서드를 사용하여 firstName 필드의 유효성 검사를 수행합니다.
+      await trigger('firstName')
+      fetch('/asdds').then(res => {
+        if (res.status == 500) {
+          throw new Error('error')
+        }
+      })
+      // trigger를 사용하여 특정 필드의 유효성 검사를 수행한 후, 필요한 추가 작업을 수행할 수 있습니다.
+      // 여기서는 유효성 검사를 통과한 후 다른 작업을 수행하도록 설정했습니다.
+      console.log('First name field is valid!')
+    } catch (error) {
+      // 유효성 검사에 실패하면 오류를 처리할 수 있습니다.
+      setError('firstName', {
+        type: 'react',
+        message: 'b입니다.'
+      })
+    }
   }
 
   // console.log(watch('firstName'))
@@ -37,7 +62,6 @@ export default function Page() {
 
   const onSubmitHandler = (e: any) => {
     console.log(getValues())
-    resetField('firstName')
     if (e.target.value.length > 4) {
       setError('firstName', {
         type: 'react',
@@ -61,13 +85,16 @@ export default function Page() {
         type="text"
         placeholder="firstName"
         {...register('firstName', {
-          required: true
+          required: true,
+          onBlur: onSubmitHandler
         })}
-        onBlur={onSubmitHandler}
       />
       {errors.firstName?.types?.manual && <p>{errors.firstName?.message}</p>}
       {/* {errors.firstName?.types?.minLength && <p>{errors.firstName?.message}</p>} */}
       {errors.firstName && errors.firstName.type === 'react' && (
+        <p>{errors.firstName.message}</p>
+      )}
+      {errors.firstName && errors.firstName.type === 'ac' && (
         <p>{errors.firstName.message}</p>
       )}
       <label>mail</label>
@@ -76,7 +103,8 @@ export default function Page() {
         {...register('mail', {
           required: '메일 입력해주세요.',
           pattern: emailRegex
-        })}></input>
+        })}
+      />
       {errors.mail && <p>올바른 이메일을 작성해주세요.</p>}
       <label>Gender Selection</label>
       <select {...register('gender')}>
